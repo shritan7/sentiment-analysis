a1d9bf6891e7d7bdf147255481478a9fa1af0c46
Shriya Tanmai <shriyatanmai1@gmail.com>
2026-07-16 22:56:23 +0530

    Add Gunicorn

--- app.py
+++ app.py
@@ -37,6 +37,8 @@ def predict():
         confidence=round(confidence, 2)
     )
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
